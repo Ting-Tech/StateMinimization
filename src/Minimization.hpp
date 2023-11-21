05973e554e4c1dbdd939324fe723f59a838f5a4b
AllenTing <67228710+Ting-Tech@users.noreply.github.com>
2023-11-21 21:08:51 +0800

    Finish  compatibility check

--- src/Minimization.hpp
+++ src/Minimization.hpp
@@ -1,14 +1,14 @@
 #include "Kiss.hpp"
 #include "Table.hpp"
 
-vector<vector<ProductTerm>> buildTable(kissData &kiss)
+std::vector<std::vector<ProductTerm>> buildTable(kissData &kiss)
 {
     char lastState = kiss.productTerms[0].currentState;
-    vector<vector<ProductTerm>> states;
+    std::vector<std::vector<ProductTerm>> states;
 
     for (size_t i = 0; i < kiss.s; i++)
     {
-        vector<ProductTerm> state;
+        std::vector<ProductTerm> state;
         for (size_t j = 0; j < (kiss.p / kiss.s); j++)
         {
             ProductTerm productTerm;
@@ -21,12 +21,12 @@ vector<vector<ProductTerm>> buildTable(kissData &kiss)
     return states;
 }
 
-vector<vector<implicantTableDataPair>>
-buildImplicantTable(vector<vector<ProductTerm>> const &table, kissData &kiss)
+std::vector<std::vector<implicantTableDataPair>>
+buildImplicantTable(std::vector<std::vector<ProductTerm>> const &table, kissData &kiss)
 {
-    vector<vector<implicantTableDataPair>>
+    std::vector<std::vector<implicantTableDataPair>>
         implicantTable(kiss.s,
-                       vector<implicantTableDataPair>(
+                       std::vector<implicantTableDataPair>(
                            kiss.s,
                            implicantTableDataPair(false,
                                                   {'X'})));
@@ -66,4 +66,46 @@ buildImplicantTable(vector<vector<ProductTerm>> const &table, kissData &kiss)
     }
 
     return implicantTable;
+}
+
+std::pair<int, int>
+isAllCompatibility(
+    std::vector<std::vector<implicantTableDataPair>> &implicantTable)
+{
+    int row = -1, column = -1;
+
+    for (size_t i = 0; i < implicantTable.size(); i++)
+    {
+        for (size_t j = 0; j < implicantTable.size(); j++)
+        {
+            if (!implicantTable[i][j].minimize)
+                continue;
+
+            int state1 = implicantTable[i][j].nextStates[0] - 'a';
+            int state2 = implicantTable[i][j].nextStates[1] - 'a';
+            int state3 = implicantTable[i][j].nextStates[2] - 'a';
+            int state4 = implicantTable[i][j].nextStates[3] - 'a';
+
+            if (implicantTable[state1][state2].minimize ||
+                implicantTable[state3][state4].minimize)
+            {
+                row = i;
+                column = j;
+                return std::make_pair(row, column);
+            }
+        }
+    }
+
+    return std::make_pair(row, column);
+}
+
+void compatibilityCheck(
+    std::vector<std::vector<implicantTableDataPair>> &implicantTable)
+{
+    pair<int, int> isCompate = isAllCompatibility(implicantTable);
+    while (isCompate.first != -1 && isCompate.second != -1)
+    {
+        implicantTable[isCompate.first][isCompate.second].minimize = false;
+        isCompate = isAllCompatibility(implicantTable);
+    }
 }
